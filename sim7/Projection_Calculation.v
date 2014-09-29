4f451c2ff52cd84df255f6279c863083d8a5e45a
jec429 <hmrbrtzero@dhcp-ccc-4748.redrover.cornell.edu>
2014-09-29 15:14:44 -0400

    C++ code of the calculation
    A necessary hint for the calculation

--- sim7/Projection_Calculation.v
+++ sim7/Projection_Calculation.v
@@ -38,7 +38,26 @@ module Projection_Calculation(
 	assign read_tracklet 	= 9'b0;
 	initial
 		write_projection = 0;
-	
+
+   /*
+    In combination with the algorithm presented in the whitepaper, this
+    is the code you will try to implement from a C++ version into verilog
+
+    Projections:
+    int it1=0.5*irproj*irinv;
+    int it2=(it1>>9)*(it1>>9);
+    int it3=(1<<20)+it2*(488);  
+    int it4=(it1>>8)*(it3>>10);
+    iphiproj=iphi0-(it4>>10); 
+    int it5=it*irproj;
+    int it6=(it5>>9)*(it3>>9);
+    izproj=iz0+(is6>>16); 
+    iphider=-0.5*irinv;
+    izder=it;
+    
+    */
+
+   
 	always @(posedge clk) begin
 		projection_calc 	<= tracklet - 1'b1;
 		write_projection 	<= 9'b0;
